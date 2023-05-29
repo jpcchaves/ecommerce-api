83eb51bdf8f22e89bdfe7c4824a0270dd04fc5a7
João Paulo <jpcchaves@outlook.com>
2023-05-29 14:03:56 -0300

    fix: add jpa annotations

--- src/main/java/com/jpcchaves/ecommerce/entity/User.java
+++ src/main/java/com/jpcchaves/ecommerce/entity/User.java
@@ -5,7 +5,11 @@ import jakarta.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY )
     private Long id;
     private String firstName;
     private String lastName;
