2d34bb10374a6b0578ee9d9a1af3388717efca66
João Paulo <jpcchaves@outlook.com>
2023-05-30 13:30:26 -0300

    refactor: adjusted return type to List

--- src/main/java/com/jpcchaves/ecommerce/repository/ProductRepository.java
+++ src/main/java/com/jpcchaves/ecommerce/repository/ProductRepository.java
@@ -3,6 +3,8 @@ package com.jpcchaves.ecommerce.repository;
 import com.jpcchaves.ecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    Product findProductByNameContainingIgnoreCase(String name);
+    List<Product> findProductsByNameContainingIgnoreCase(String name);
 }
