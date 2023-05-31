48f46ee00845f0e3dbf87fb70c609ec0880cdc64
João Paulo <jpcchaves@outlook.com>
2023-05-31 09:01:43 -0300

    feat: add category controller

---
+++ src/main/java/com/jpcchaves/ecommerce/controller/CategoryController.java
@@ -0,0 +1,27 @@
+package com.jpcchaves.ecommerce.controller;
+
+import com.jpcchaves.ecommerce.entity.Category;
+import com.jpcchaves.ecommerce.service.CategoryService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/categories")
+public class CategoryController {
+
+    private final CategoryService service;
+
+    public CategoryController(CategoryService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Category>> getAll() {
+        return ResponseEntity.status(HttpStatus.OK).body(service.getAll());
+    }
+}
