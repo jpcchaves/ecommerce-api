1de910162c04ce7c8855183b3373b7518a95bd96
João Paulo <jpcchaves@outlook.com>
2023-05-31 12:27:56 -0300

    feat: add new attributes to sub category

--- src/main/java/com/jpcchaves/ecommerce/entity/SubCategory.java
+++ src/main/java/com/jpcchaves/ecommerce/entity/SubCategory.java
@@ -1,6 +1,10 @@
 package com.jpcchaves.ecommerce.entity;
 
 import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.util.Date;
 
 @Entity
 @Table(name = "sub_categories")
@@ -10,13 +14,22 @@ public class SubCategory {
     private Long id;
     private String name;
 
+    @CreationTimestamp
+    private Date createdAt;
+    @UpdateTimestamp
+    private Date updatedAt;
+
     public SubCategory() {
     }
 
     public SubCategory(Long id,
-                       String name) {
+                       String name,
+                       Date createdAt,
+                       Date updatedAt) {
         this.id = id;
         this.name = name;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
     }
 
     public Long getId() {
@@ -34,5 +47,20 @@ public class SubCategory {
     public void setName(String name) {
         this.name = name;
     }
-}
 
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
